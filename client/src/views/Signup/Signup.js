1263dbfbd0b20fa9d2754cda58d2f50a37c23ecf
AvishkarMandlik <mandlikavi121@gmail.com>
2023-01-13 14:41:39 +0530

    signup page done

--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from 'axios';
 import "./Signup.css";
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+
 
 
 function Signup() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState('user')
+
+  async function signupUser(){
+    const response = await axios.post('/signup',{
+      name: name,
+      email: email,
+      phone: phone,
+      password: password,
+      role: role
+    })
+    console.log(response.data)
+    if(response.data.success){
+      alert(response.data.message)
+      window.location.href = '/login '
+    }
+    else{
+      alert(response.data.message)
+      setName('')
+      setEmail('')
+      setPhone('')
+      setPassword('')
+    }
+  }
+
   return (
     <div className="main-container">
       <div>
@@ -19,26 +49,38 @@ function Signup() {
           <form>
            <div>
               <label htmlFor='name'> </label>
-              <input type='text' id='name'  placeholder='Enter Your FullName'  className='user-input'/>
+              <input type='text' id='name' 
+               placeholder='Enter Your FullName' 
+               className='user-input' 
+               value={name} onChange={(e)=> setName(e.target.value)}/>
            </div>
 
            <div>
               <label htmlFor='email'> </label>
-              <input type='email' id='email' placeholder='Enter Your Email' className='user-input'/>
+              <input type='email' id='email'
+               placeholder='Enter Your Email' 
+               className='user-input' 
+               value={email} onChange={(e)=> setEmail(e.target.value)}/>
            </div>
 
            <div>
               <label htmlFor='phone'> </label>
-              <input type='text' id='phone' placeholder='Enter Your Phone' className='user-input'/>
+              <input type='text' id='phone'
+               placeholder='Enter Your Phone'
+               className='user-input' 
+               value={phone} onChange={(e)=> setPhone(e.target.value)}/>
            </div>
 
            <div>
               <label htmlFor='password'> </label>
-              <input type='password' id='password' placeholder='Enter Your Password' className='user-input'/>
+              <input type='password' id='password'
+               placeholder='Enter Your Password'
+               className='user-input' 
+               value={password} onChange={(e)=> setPassword(e.target.value)}/>
            </div>
 
             <div>
-              <button type='button' className='signup-button '>Signup</button>
+              <button type='button' className='signup-button' onClick={signupUser}>Signup</button>
             </div>
             <div className="already-login-container">
               <span>Already have an acount ? 
