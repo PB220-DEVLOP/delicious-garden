2d0fe644f0e5ae196d6825206cc9adf932920eda
AvishkarMandlik <mandlikavi121@gmail.com>
2023-01-15 21:03:33 +0530

    home page logout btn and function added

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { currentUser } from '../../util/currentUser';
 
 function Home() {
+  function LogOut(){
+    localStorage.removeItem('currentUser');
+    window.location.href = "/login"
+  }
+
+  if(!currentUser){
+    alert('Login is required')
+    window.location.href = "/login"
+  }
+
   return (
-    <>
+    <div>
     <div>Home Component</div>
     <h2>{currentUser?.name}</h2>
-    </>
+
+    <button type="button" className="btn btn-primary" onClick={LogOut}>Logout</button>
+    </div>
   )
 }
 
