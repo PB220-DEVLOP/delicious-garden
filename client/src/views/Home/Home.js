538cda73a8c2d953945b8b8b5c75793319b8093e
AvishkarMandlik <mandlikavi121@gmail.com>
2023-01-16 11:52:59 +0530

    search container added

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { currentUser } from '../../util/currentUser';
-
+import './Home.css';
 
 function Home() {
   function LogOut(){
@@ -17,6 +17,10 @@ function Home() {
     <div>
     <div>Home Component</div>
     <h2>{currentUser?.name}</h2>
+
+    <div className="search-container">
+      <input type="text" placeholder='search' className='input-search'/>
+    </div>
 
     <button type="button" className="btn btn-primary" onClick={LogOut}>Logout</button>
     </div>
