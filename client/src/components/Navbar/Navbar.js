d3254d14e17a3ab50096dd30addaee197b30e9c6
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-03 22:35:45 +0530

    navbar bg color change and search container plachode add

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,16 +7,16 @@ function Navbar({ user }) {
   const [foodItemCount, setFoodItemCount] = useState(myFoodListCount)
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <nav class="navbar navbar-expand-lg navbar-dark  bg-secondary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Delicious Garden 🍔</a>
+          <Link class="navbar-brand" href="#">Delicious Garden 🍔</Link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <Link class="nav-link active" aria-current="page" href="home">Home</Link>
               </li>
             </ul>
             <form class="d-flex align-items-center">
