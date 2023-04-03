49c457dcb303d1263a4b7138c7a2732066a0968b
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-03 16:11:41 +0530

    add to list and increment decrement btns added

--- client/src/components/FoodItemCard/FoodItemCard.js
+++ client/src/components/FoodItemCard/FoodItemCard.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './FoodItemCard.css';
+import swal from 'sweetalert'
 
 function FoodItemCard({category,description,imgUrl,price,title}) {
+  
+  const [quantity, setQuantity] = useState(1)
+
+  async function addToList(){
+   const listObject = {
+     name: title,
+     price: price,
+     quantity: quantity
+   }
+
+   const existingList = JSON.parse(localStorage.getItem('list')) || []
+
+   existingList.push(listObject)
+
+   localStorage.setItem('list', JSON.stringify(existingList))
+
+   await swal({
+      title: "Added to List",
+      icon: "success",
+   })
+   window.location.reload()
+  }
+
   return (
     <div className='col-md-3'>
         <div className='food-item-card'>
@@ -12,8 +36,20 @@ function FoodItemCard({category,description,imgUrl,price,title}) {
             <p>{description}</p>
             <p>{price}/- Only</p>
             <span>{category}</span>
+
+            <div className='quantity-btn-container'>
+          <span className='qnt-btn' onClick={(e)=>{setQuantity(quantity-1)}}>-</span>
+          <span className='qnt-text'>{quantity}</span>
+          <span className='qnt-btn' onClick={(e)=>{setQuantity(quantity+1)}}>+</span>
+        </div>
+
+        <div>
+          <button type="button" className='btn-add-to-list' onClick={addToList}>Add To List</button>
+        </div>
         </div>
     </div>
+
+    
   )
 }
 
