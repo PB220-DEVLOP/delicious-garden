484bf43ec64bb4669d29e3a59243fb12376aa3e2
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-03 23:05:45 +0530

    Booktable route set

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from './views/Home/Home';
 import Login from './views/Login/Login';
 import Signup from './views/Signup/Signup';
+import BookTable from './views/BookTable/BookTable';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/BookTable" element={<BookTable/>}/>
        </Routes>
       </BrowserRouter>
     </div>
