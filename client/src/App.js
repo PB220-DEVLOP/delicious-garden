18a1f0e7099f8913efd7d0c1285b4888554c2271
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-06 22:17:19 +0530

    my list route set

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from './views/Login/Login';
 import Signup from './views/Signup/Signup';
 import BookTable from './views/BookTable/BookTable';
 import MyOrders from './views/MyOrders/MyOrders';
+import MyList from './views/MyList/MyList';
 import Profile from './views/Profile/Profile';
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/BookTable" element={<BookTable/>}/>
-        <Route path="/myList" element={<myList />} />
+        <Route path="/MyList" element={<MyList />} />
         <Route path="/MyOrders" element={<MyOrders />} />
         <Route path="/Profile" element={<Profile />} />
        </Routes>
